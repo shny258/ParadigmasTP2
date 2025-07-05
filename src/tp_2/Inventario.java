463b573a556cff310d5e158be0edcca96bb7b3b5
Shny <luciano_258456@hotmail.com>
2025-07-05 13:56:24 -0300

    mas tests InventarioTest

--- src/tp_2/Inventario.java
+++ src/tp_2/Inventario.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import prolog.ManejoProlog;
 
@@ -222,6 +223,23 @@ public class Inventario {
 		}
 		cadenaRet += "}";
 		return cadenaRet;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(objetos);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Inventario other = (Inventario) obj;
+		return Objects.equals(objetos, other.objetos);
 	}
 
 	@Override
--- test/tp_2/InventarioTest.java
+++ test/tp_2/InventarioTest.java
@@ -1,6 +1,5 @@
 package tp_2;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
@@ -12,20 +11,18 @@ import org.junit.jupiter.api.Test;
 import prolog.ManejoProlog;
 
 class InventarioTest {
-	
-
-	String pathRecetasTest= "archivos/testrecetas.json";
+	String pathRecetasTest = "archivos/testrecetas.json";
 	String pathInventarioTest = "archivos/testinventario.json";
 	String pathInventarioJsonOut = "inventarioJsonOut.json";
 	Inventario inventarioJsonTest;
 	RegistroObjetos registroObjetosTest;
 	ManejadorArchivos manejador;
 	ManejoProlog pl = ManejoProlog.getInstance(pathRecetasTest, "archivos/reglasProlog.txt");
-	
+
 	@BeforeEach
 	public void setUp() {
 		registroObjetosTest = new RegistroObjetos();
-		manejador = new ManejadorArchivos();		
+		manejador = new ManejadorArchivos();
 		try {
 			manejador.cargarRecetasDesdeJson(pathRecetasTest, registroObjetosTest);
 		} catch (Exception e) {
@@ -38,30 +35,99 @@ class InventarioTest {
 			System.err.println("ERROR AL CARGAR EL INVENTARIO TEST");
 			return;
 		}
-		
+
 	}
-	
+
 	@Test
-	void CrearInventarioNoFalla() {
+	void crearInventarioNoFalla() {
 		Objeto marco = registroObjetosTest.obtenerObjeto("Marco");
 		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
 		Objeto tablon = registroObjetosTest.obtenerObjeto("Tablon");
 		Objeto carbon = registroObjetosTest.obtenerObjeto("Carbon");
 		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
 		Map<Objeto, Integer> objetos = new HashMap<>();
+		Map<Objeto, Integer> objetosVacio = new HashMap<>();
 		objetos.put(marco, 10);
 		objetos.put(palo, 5);
 		objetos.put(tablon, 6);
-		objetos.put(carbon, 20);;
-		objetos.put(madera, 20);;
-		
+		objetos.put(carbon, 20);
+		objetos.put(madera, 20);
+
 		Inventario inventarioCreado = new Inventario(objetos);
-		assertEquals(objetos,inventarioCreado.getObjetos());
+		Inventario inventarioVacio = new Inventario();
+		assertEquals(objetos, inventarioCreado.getObjetos());
+		assertEquals(objetosVacio, inventarioVacio.getObjetos());
 	}
+
+	@Test
+	void agregarObjetoTest() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Inventario inv = new Inventario();
+		Map<Objeto, Integer> objetos = new HashMap<>();
+		objetos.put(madera, 20);
+		inv.agregar(madera, 20);
+		assertEquals(objetos, inv.getObjetos());
+	}
+
+	@Test
+	void agregarSumarObjetoTest() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Inventario inv = new Inventario();
+		Map<Objeto, Integer> objetos = new HashMap<>();
+		objetos.put(madera, 40);
+		inv.agregarSumar(madera, 20);
+		inv.agregarSumar(madera, 20);
+		assertEquals(objetos, inv.getObjetos());
+	}
+
+	@Test
+	void sacarObjetoTest() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Inventario inv = new Inventario();
+		Map<Objeto, Integer> objetos = new HashMap<>();
+		inv.agregar(madera, 20);
+		inv.sacar(madera);
+		assertEquals(objetos, inv.getObjetos());
+	}
+
+	@Test
+	void sacarObjetoTest_False() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
+		Inventario inv = new Inventario();
+		inv.agregar(madera, 20);
+		assertFalse(inv.sacar(palo));
+	}
+
+	@Test
+	void sacarRestarObjetoTest() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Inventario inv = new Inventario();
+		Map<Objeto, Integer> objetos = new HashMap<>();
+		objetos.put(madera, 20);
+		inv.agregarSumar(madera, 20);
+		inv.agregarSumar(madera, 20);
+		inv.sacarRestar(madera, 20);
+		assertEquals(objetos, inv.getObjetos());
+	}
+
+	@Test
+	void sacarRestarObjetoTest_False() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
+		Inventario inv = new Inventario();
+		inv.agregarSumar(madera, 20);
+		inv.agregarSumar(madera, 20);
+		assertFalse(inv.sacarRestar(palo, 20));
+		assertFalse(inv.sacarRestar(madera, 50));
+		assertTrue(inv.sacarRestar(madera, 40));
+	}
+
 	@Test
 	void mostrarInventarioTestEsperado() {
-		assertEquals("{Marco=1, Palo=2, Tablon=2, Carbon=4}",inventarioJsonTest.toString());
+		assertEquals("{Marco=1, Palo=2, Tablon=2, Carbon=4}", inventarioJsonTest.toString());
 	}
+
 	@Test
 	void faltanteParaCraftearTestEsperado() {
 		Objeto antorcha = registroObjetosTest.obtenerObjeto("Antorcha");
@@ -69,8 +135,9 @@ class InventarioTest {
 		Map<Objeto, Integer> ingredientes = new HashMap<>();
 		ingredientes.put(palo, 3);
 		Receta faltanteParaCrearAntorcha = new Receta(9, 1, ingredientes);
-		assertEquals(faltanteParaCrearAntorcha,inventarioJsonTest.faltantesParaCraftear(antorcha));
+		assertEquals(faltanteParaCrearAntorcha, inventarioJsonTest.faltantesParaCraftear(antorcha));
 	}
+
 	@Test
 	void getObjetosEsperado() {
 		Objeto marco = registroObjetosTest.obtenerObjeto("Marco");
@@ -82,8 +149,9 @@ class InventarioTest {
 		objetos.put(palo, 2);
 		objetos.put(tablon, 2);
 		objetos.put(carbon, 4);
-		assertEquals(objetos,inventarioJsonTest.getObjetos());
+		assertEquals(objetos, inventarioJsonTest.getObjetos());
 	}
+
 	@Test
 	void faltanteParaCraftearDesdeCeroTestEsperado() {
 		Objeto antorcha = registroObjetosTest.obtenerObjeto("Antorcha");
@@ -91,22 +159,24 @@ class InventarioTest {
 		Map<Objeto, Integer> ingredientes = new HashMap<>();
 		ingredientes.put(madera, 6);
 		Receta faltanteParaCrearAntorchaDeCero = new Receta(9, 1, ingredientes);
-		assertEquals(faltanteParaCrearAntorchaDeCero,inventarioJsonTest.faltantesParaCraftearDeCero(antorcha));
+		assertEquals(faltanteParaCrearAntorchaDeCero, inventarioJsonTest.faltantesParaCraftearDeCero(antorcha));
 	}
+
 	@Test
 	void craftearMesaTestPuede() {
 		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
 		Objeto tablon = registroObjetosTest.obtenerObjeto("Tablon");
 		Objeto carbon = registroObjetosTest.obtenerObjeto("Carbon");
-		Map<Objeto, Integer> inventarioEsperado = new HashMap<>();
-		inventarioEsperado.put(carbon, 4);
-		inventarioEsperado.put(tablon, 1);
-		inventarioEsperado.put(mesa, 1);
-		
+		Inventario inventarioEsperado = new Inventario();
+		inventarioEsperado.agregar(carbon, 4);
+		inventarioEsperado.agregar(tablon, 1);
+		inventarioEsperado.agregar(mesa, 1);
+
 		assertTrue(inventarioJsonTest.craftear(mesa));
-		assertEquals(inventarioEsperado,inventarioJsonTest.getObjetos());
-		assertEquals("{1=Mesa}",inventarioJsonTest.getHistorial().toString());	
+		assertEquals(inventarioEsperado, inventarioJsonTest);
+		assertEquals("{1=Mesa}", inventarioJsonTest.getHistorial().toString());
 	}
+
 	@Test
 	void craftearTornilloTestNoPuede() {
 		Objeto tornillo = registroObjetosTest.obtenerObjeto("Tornillo");
@@ -114,82 +184,70 @@ class InventarioTest {
 		Objeto marco = registroObjetosTest.obtenerObjeto("Marco");
 		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
 		Objeto carbon = registroObjetosTest.obtenerObjeto("Carbon");
-		Map<Objeto, Integer> inventarioEsperado = new HashMap<>();
-		inventarioEsperado.put(marco, 1);
-		inventarioEsperado.put(palo, 2);
-		inventarioEsperado.put(tablon, 2);
-		inventarioEsperado.put(carbon, 4);
-		
-		assertFalse(inventarioJsonTest.craftear(tornillo));
-		assertEquals(inventarioEsperado,inventarioJsonTest.getObjetos());
-		assertEquals("{}",inventarioJsonTest.getHistorial().toString());	
-	}
-	
-	@Test
-	void cuantosPuedeCraftearTest_CraftearDosMesasSoloBasicos() {		
-		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
-		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
-		Map<Objeto, Integer> inventarioActual = new HashMap<>();
+		Inventario inventarioEsperado = new Inventario();
+		inventarioEsperado.agregar(marco, 1);
+		inventarioEsperado.agregar(palo, 2);
+		inventarioEsperado.agregar(tablon, 2);
+		inventarioEsperado.agregar(carbon, 4);
 
-		inventarioActual.put(madera, 57);
-		Inventario inv = new Inventario(inventarioActual);
-		
-		assertEquals(2,inv.cuantosPuedoCraftear(mesa));
+		assertFalse(inventarioJsonTest.craftear(tornillo));
+		assertEquals(inventarioEsperado, inventarioJsonTest);
+		assertEquals("{}", inventarioJsonTest.getHistorial().toString());
 	}
-	
+
 	@Test
-	void cuantosPuedeCraftearTest_CraftearDosMesasConIntermediosVarios() {		
+	void cuantosPuedeCraftearTest_CraftearDosMesasSoloBasicos() {
+		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
+		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
+		Inventario inv = new Inventario();
+		inv.agregar(madera, 57);
+		assertEquals(2, inv.cuantosPuedoCraftear(mesa));
+	}
+
+	@Test
+	void cuantosPuedeCraftearTest_CraftearDosMesasConIntermediosVarios() {
 		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
 		Objeto marco = registroObjetosTest.obtenerObjeto("Marco");
 		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
 		Objeto tablon = registroObjetosTest.obtenerObjeto("Tablon");
 		Objeto tornillo = registroObjetosTest.obtenerObjeto("Tornillo");
 		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
-		Map<Objeto, Integer> inventarioActual = new HashMap<>();
-		inventarioActual.put(madera, 2);
-		inventarioActual.put(marco, 1);
-		inventarioActual.put(tablon, 3);
-		inventarioActual.put(tornillo, 2);
-		inventarioActual.put(palo, 15);
-		
-		Inventario inv = new Inventario(inventarioActual);
-		
-		assertEquals(2,inv.cuantosPuedoCraftear(mesa));
+		Inventario inv = new Inventario();
+		inv.agregar(madera, 2);
+		inv.agregar(marco, 1);
+		inv.agregar(tablon, 3);
+		inv.agregar(tornillo, 2);
+		inv.agregar(palo, 15);
+		assertEquals(2, inv.cuantosPuedoCraftear(mesa));
 	}
-	
+
 	@Test
-	void cuantosPuedeCraftearTest_CraftearDosMesasConIntermediosTornillos() {		
+	void cuantosPuedeCraftearTest_CraftearDosMesasConIntermediosTornillos() {
 		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
 		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
 		Objeto tornillo = registroObjetosTest.obtenerObjeto("Tornillo");
 		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
-		Map<Objeto, Integer> inventarioActual = new HashMap<>();
-		inventarioActual.put(madera, 45);
-		inventarioActual.put(tornillo, 200);
-		inventarioActual.put(palo, 5);
-		
-		Inventario inv = new Inventario(inventarioActual);
-		
-		assertEquals(2,inv.cuantosPuedoCraftear(mesa));
+		Inventario inv = new Inventario();
+		inv.agregar(madera, 45);
+		inv.agregar(tornillo, 200);
+		inv.agregar(palo, 5);
+		assertEquals(2, inv.cuantosPuedoCraftear(mesa));
 	}
-	
+
 	@Test
-	void cuantosPuedeCraftearTest_CraftearTresMesasConIntermediosVarios() {		
+	void cuantosPuedeCraftearTest_CraftearTresMesasConIntermediosVarios() {
 		Objeto madera = registroObjetosTest.obtenerObjeto("Madera");
 		Objeto marco = registroObjetosTest.obtenerObjeto("Marco");
 		Objeto palo = registroObjetosTest.obtenerObjeto("Palo");
 		Objeto tablon = registroObjetosTest.obtenerObjeto("Tablon");
 		Objeto tornillo = registroObjetosTest.obtenerObjeto("Tornillo");
 		Objeto mesa = registroObjetosTest.obtenerObjeto("Mesa");
-		Map<Objeto, Integer> inventarioActual = new HashMap<>();
-		inventarioActual.put(madera, 60);
-		inventarioActual.put(marco, 1);
-		inventarioActual.put(tablon, 1);
-		inventarioActual.put(tornillo, 1);
-		inventarioActual.put(palo, 1);
-		
-		Inventario inv = new Inventario(inventarioActual);
-		
-		assertEquals(3,inv.cuantosPuedoCraftear(mesa));
+		Inventario inv = new Inventario();
+		inv.agregar(madera, 60);
+		inv.agregar(marco, 1);
+		inv.agregar(tablon, 1);
+		inv.agregar(tornillo, 1);
+		inv.agregar(palo, 1);
+		assertEquals(3, inv.cuantosPuedoCraftear(mesa));
 	}
 }
